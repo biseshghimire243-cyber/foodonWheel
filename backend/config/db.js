02b617533b82f1f037ace9cb89bc206acb11a381
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-02 08:32:08 -0700

    the databse are done

---
+++ backend/config/db.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("❌ Database Connection Failed:", err.message);
+        return;
+    }
+
+    console.log("✅ MySQL Database Connected Successfully");
+    connection.release();
+});
+
+module.exports = pool.promise();
