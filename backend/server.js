260de56d30ce6d866b43cfe4b94006473dd58ab4
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-02 08:40:42 -0700

    the new routw s are done

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
 
 // =========================
 // DATABASE
 // =========================
 
 const db = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 // =========================
 // FRONTEND
